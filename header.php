13a6106d6912275596b6537c7d8a7e065f0b660a
NVCDevelopmentTeam <Quang.hnm.hd.2k@gmail.com>
2022-01-22 19:59:08 +0700

    Source code
    Here is the source code of the theme

---
+++ header.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		            <meta charset="<?php bloginfo( 'charset' ); ?>">
+		<title>Noise Media
+            <?php wp_title( '|', true, 'right' ); ?>
+            <?php bloginfo('name'); ?>
+</title>
+            <link rel="profile" href="http://gmpg.org/xfn/11">
+            <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+            <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
+            <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/mystyle.css" type="text/css" >
+            <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/bootstrap.css" type="text/css" media="screen">
+		<style>
+		</style>
+            <?php wp_head(); ?>
+	</head>
+	<body>
+		<div id="top-bar"></div>
+
+		<div class="normal-wrapper">
+			<div id="logo-container">
+				<i class="fa fa-volume-down logo-icon"></i><h1>Noise Media</h1>
+			</div>
+<div  id="mainmenu">
+			<ul id="navbar">
+				<li><a href="" Accesskey=h>Home</a></li>
+				<li><a href="" accesskey=a>About</a></li>
+				<li><a href="" accesskey=r>Reviews</a></li>
+				<li><a href="" class="last-link" accesskey=c>Contact</a></li>
+			</ul>
+<b>"Font size:"</b> 
+<button id="btn-decrease" class="sticky-btn btn-dec btn" title="Reduce font size">A</button>
+<button id="btn-orig" class="sticky-btn btn-orig btn" title="Restore font size">A</button>
+<button id="btn-increase" class="sticky-btn btn-inc btn" title="Increase font size">A</button>
+
+		</div>
+		</div>
+
+		<div id="top-color-splash"></div>
